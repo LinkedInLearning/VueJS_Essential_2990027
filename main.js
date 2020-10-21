9861371e533710e21f2e2dc3236d770ad9fafc2b
mahedes <mahevadessart@gmail.com>
2020-10-21 23:53:58 +0200

    01_06

--- main.js
+++ main.js
@@ -5,7 +5,37 @@ const app = Vue.createApp({
       price: 12,
       image: "assets/images/pizza1-tomate.jpg",
       sale: false,
-      notAvailable: true
+      notAvailable: true,
+      ingredients: [
+        "Olives",
+        "Poulet roti",
+        "Bacon",
+        "Poivrons",
+        "Champignons",
+        "Mozzarella",
+        "Oeuf",
+      ],
+      sauces: [
+        {
+          id: 1001,
+          type: "Sauce Tomate",
+          color: "#db4006",
+          image: "assets/images/pizza1-tomate.jpg",
+        },
+        {
+          id: 1002,
+          type: "Crème Fraiche",
+          color: "#e9cb8f",
+          image: "assets/images/pizza1-creme.jpg.jpg",
+        },
+      ],
+      energy: {
+        Kcal: 242,
+        Glucides: 27.99,
+        Fibres: 1.75,
+        Proteines: 9.62,
+        Sel: 11,
+      },
     }
   }
 })
