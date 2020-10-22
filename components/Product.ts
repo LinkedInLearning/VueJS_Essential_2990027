879e34f4b1582cc0e063797455040041c40b3d46
mahedes <mahevadessart@gmail.com>
2020-10-22 18:17:55 +0200

    03_01_f

---
+++ components/Product.ts
@@ -0,0 +1,114 @@
+app.component("product", {
+
+  data() {
+    return {
+      product: "Pizza",
+      type: "Orientale",
+      price: 12,
+      image: "assets/images/pizza1-tomate.jpg",
+      sale: true,
+      notAvailable: true,
+      ingredients: [
+        "Olives",
+        "Poulet roti",
+        "Bacon",
+        "Poivrons",
+        "Champignons",
+        "Mozzarella",
+        "Oeuf",
+      ],
+      sauces: [
+        {
+          id: 1001,
+          type: "Sauce Tomate",
+          color: "#db4006",
+          image: "assets/images/pizza1-tomate.jpg",
+        },
+        {
+          id: 1002,
+          type: "Crème Fraiche",
+          color: "#e9cb8f",
+          image: "assets/images/pizza1-creme.jpg",
+        },
+      ],
+      energy: {
+        Kcal: 242,
+        Glucides: 27.99,
+        Fibres: 1.75,
+        Proteines: 9.62,
+        Sel: 11,
+      },
+    }
+  },
+
+  methods: {
+    addProduct() {
+      if (this.sale) {
+        this.nbrProduct += 1;
+        this.totalPrice += this.price - 5;
+      } else {
+        this.nbrProduct += 1;
+        this.totalPrice += this.price;
+      }
+    },
+    updateImage(newLinkImage) {
+      this.image = newLinkImage;
+    }
+  },
+
+  computed: {
+    title() {
+      return this.product + " " + this.type;
+    },
+  },
+
+  template:
+    `<!-- Image du produit -->
+    <div class="product-image">
+      <img :src="image" />
+    </div>
+
+    <!-- Description du produit -->
+    <div class="product-description">
+      <h1>{{ title }}</h1>
+      <p v-show="notAvailable">Momentanément indisponible</p>
+
+      <p v-if="sale">
+        <span class="sale">{{ price }} € </span>
+        <span class="new-price"> {{ price - 5}} € PROMOTION</span>
+      </p>
+      <p v-else>
+        <span class="price">{{ price }} €</span>
+      </p>
+
+      <br />
+      <strong>Ingrédients </strong>
+      <div>
+        <span v-for="ingredient in ingredients">{{ ingredient + ", "}}
+        </span>
+      </div>
+      <br />
+      <div class="sauces">
+        <strong>Sauces au choix</strong>
+        <ul>
+          <li v-for="sauce in sauces" @mouseover="updateImage(sauce.image)" :style="{ backgroundColor: sauce.color }">
+            {{ sauce.type }}
+          </li>
+        </ul>
+      </div>
+      <div>
+        <strong>Valeurs nutritionnelles pour 100 grammes</strong>
+        <ul>
+          <li v-for="(value, name) in energy">
+            {{ name }} : {{ value }}
+          </li>
+        </ul>
+      </div>
+
+      <!-- Bouton d'ajout au panier -->
+      <button :class="{ notActiveBtn : notAvailable}" @click="addProduct()" :disabled="notAvailable">
+        Ajouter à ma commande
+      </button>
+      <br /><br />
+    </div>`
+})
--- main.js
+++ main.js
@@ -1,66 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: "Pizza",
-      type: "Orientale",
-      price: 12,
-      image: "assets/images/pizza1-tomate.jpg",
-      sale: true,
-      notAvailable: true,
-      ingredients: [
-        "Olives",
-        "Poulet roti",
-        "Bacon",
-        "Poivrons",
-        "Champignons",
-        "Mozzarella",
-        "Oeuf",
-      ],
-      sauces: [
-        {
-          id: 1001,
-          type: "Sauce Tomate",
-          color: "#db4006",
-          image: "assets/images/pizza1-tomate.jpg",
-        },
-        {
-          id: 1002,
-          type: "Crème Fraiche",
-          color: "#e9cb8f",
-          image: "assets/images/pizza1-creme.jpg",
-        },
-      ],
-      energy: {
-        Kcal: 242,
-        Glucides: 27.99,
-        Fibres: 1.75,
-        Proteines: 9.62,
-        Sel: 11,
-      },
       totalPrice: 0,
       nbrProduct: 0,
       promo: "",
     }
-  },
-
-  methods: {
-    addProduct() {
-      if (this.sale) {
-        this.nbrProduct += 1;
-        this.totalPrice += this.price - 5;
-      } else {
-        this.nbrProduct += 1;
-        this.totalPrice += this.price;
-      }
-    },
-    updateImage(newLinkImage) {
-      this.image = newLinkImage;
-    }
-  },
-
-  computed: {
-    title() {
-      return this.product + " " + this.type;
-    },
   }
 })
