2d5c590b78abb859aa2904b96d571c15ae458bf2
mahedes <mahevadessart@gmail.com>
2020-10-25 23:59:53 +0100

    update 05_02_f

--- vuejs3-linkedin-project/src/App.vue
+++ vuejs3-linkedin-project/src/App.vue
@@ -30,7 +30,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 a {
   background: black;
   color: #ffffff;
--- vuejs3-linkedin-project/src/components/Home.vue
+++ vuejs3-linkedin-project/src/components/Home.vue
@@ -25,7 +25,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 #intro {
   background-image: url(../assets/images/pizza-menu.jpg);
   background-position: bottom;
