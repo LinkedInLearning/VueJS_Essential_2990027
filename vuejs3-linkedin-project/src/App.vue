75294f3741c07bfd133cb88a9edb177dc3c4bca3
mahedes <mahevadessart@gmail.com>
2020-10-25 11:32:45 +0100

    04_03_f

--- vuejs3-linkedin-project/src/App.vue
+++ vuejs3-linkedin-project/src/App.vue
@@ -1,26 +1,10 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-</template>
+<template></template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ vuejs3-linkedin-project/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template> </template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+
+  methods: {},
+
+  computed: {}
+};
+</script>
+
+<style>
+</style>
