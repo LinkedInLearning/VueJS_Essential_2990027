94addd90e023b275e585748016657f2cf872ffa7
mahedes <mahevadessart@gmail.com>
2020-10-25 23:56:59 +0100

    05_02_f

--- vuejs3-linkedin-project/src/main.js
+++ vuejs3-linkedin-project/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router";
 import Products from "./components/Products.vue";
 
 const app = createApp(App);
 app.component("Products", Products);
+app.use(router);
 app.mount("#app");
